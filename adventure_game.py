a991980e9e7af466d018a8d2879cf48796fccf17
F/A-18A <89731702+LynchDeclan@users.noreply.github.com>
2023-03-22 01:10:27 +0300

    Starting Idea

---
+++ adventure_game.py
@@ -0,0 +1,42 @@
+import time
+import sys
+
+def main():
+    figure_questions = ["'Where am I?'", "'Who are you?'", "'What happened to me?'", "'What lies beyond this gate?'"]
+    time.sleep(5)
+    print("You awaken, not knowing where you are and having no memory of past events...")
+    time.sleep(3)
+    print("You observe a barren landscape, the horizon touching an infinitely darkened land...")
+    time.sleep(3)
+    print("the faint glimmer of the rays of a mysterious light source is the only thing keeping you from not being able to see...")
+    time.sleep(3)
+    print("But you are drawn to an obscured structure in the distance, beckoning you to come near...")
+    time.sleep(2)
+    print("And you approach what appears to be a gate. Worn. Rusted. It even seems as though it would be venomous to the touch, yet you do not understand why...")
+    time.sleep(4)
+    print("A deep voice breaks the silence, and you understand the spoken words:")
+    time.sleep(1)
+    print("'Someone at the door, worthy of the gift of Death, but do they lack courage to accept it?'")
+    time.sleep(4)
+    escape_choice = input("You are suddenly afraid, but do you choose to run?")
+    if escape_choice == "yes":
+        time.sleep(4)
+        print("You ran as fast as your crumbling legs could take you. You did not choose to accept the gift that the mysterious figure spoke of...")
+        time.sleep(2)
+        print("In the world beyond, blackened ichor filled a crumbling sky, as what was left of your sould withered to nothing...")
+        time.sleep(5)
+        sys.exit()
+    if escape_choice == "no":
+        time.sleep(4)
+        print("You decide to push your fear back inside of you. You then wish to question the mysterious figure...")
+        time.sleep(3)
+        for question in figure_questions:
+            print(str(question))
+        time.sleep(2)
+        figure_asking = input("What question do you ask this mysterious figure?")
+        
+
+
+
+if __name__ == "__main__":
+    main()

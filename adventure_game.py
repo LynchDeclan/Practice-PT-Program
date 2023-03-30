8cc7a4afdd7ddc1739a0bc627e7dd8f45e8c38f2
F/A-18A <89731702+LynchDeclan@users.noreply.github.com>
2023-03-30 20:10:07 +0300

    F

--- adventure_game.py
+++ adventure_game.py
@@ -3,31 +3,31 @@ import sys
 
 def main():
     figure_questions = ["'Where am I?'", "'Who are you?'", "'What happened to me?'", "'What lies beyond this gate?'"]
-    time.sleep(4)
+    time.sleep(3)
     print("You awaken, not knowing where you are and having no memory of past events...")
-    time.sleep(3)
+    time.sleep(2)
     print("You observe a barren landscape, the horizon touching an infinitely darkened land...")
-    time.sleep(3)
+    time.sleep(2)
     print("the faint glimmer of the rays of a mysterious light source is the only thing keeping you from not being able to see...")
-    time.sleep(3)
+    time.sleep(2)
     print("But you are drawn to an obscured structure in the distance, beckoning you to come near...")
     time.sleep(2)
     print("And you approach what appears to be a gate. Worn. Rusted. It even seems as though it would be venomous to the touch, yet you do not understand why...")
-    time.sleep(4)
+    time.sleep(3)
     print("A deep voice breaks the silence, and you understand the spoken words:")
     time.sleep(1)
     print("'Someone at the door, worthy of the gift of Death, but do they lack the courage to accept it?'")
-    time.sleep(4)
+    time.sleep(3)
     escape_choice = input("You are suddenly afraid, but do you choose to run?\n")
     if escape_choice[0] in ["Y" or "y"]:
-        time.sleep(4)
+        time.sleep(3)
         print("You ran as fast as your crumbling legs could take you. You did not choose to accept the gift that the mysterious figure spoke of...")
         time.sleep(2)
         print("In the world beyond, blackened ichor filled a crumbling sky, as the remnants of your sould withered to nothing...")
-        time.sleep(5)
+        time.sleep(4)
         sys.exit()
     if escape_choice[0] in ["N" or "n"]:
-        time.sleep(4)
+        time.sleep(3)
         name = str(input("You decide to push your fear back inside of you. The mysterious figure suddenly speaks again: 'What is your name, mortal?'\n"))
         time.sleep(2)
         print("You tell the mysteriosu figure a name you suddenly remembered, in that moment, as your own: " +name+ " but it doesn't really feel like you remember...")
@@ -52,28 +52,28 @@ def first_questions(figure_questions, name):
          who_are_you()
     
     if asking_figure == "What happened to me?":
-        what_happened_to_me(figure_questions)
+        what_happened_to_me(figure_questions, asking_figure, question)
 
     if asking_figure == "What lies beyond this gate?":
         what_lies_beyond_gate()
      
     
 def where_am_i():
-     time.sleep(5)
+     time.sleep(4)
      print("The msyteriosu figure speaks: 'Even I do not know or understand...'")
      time.sleep(1)
      print("'All I ever knew and will know of this dark place is misery, and how it is known as The Sunless Lands. Mortal, you are no longer on the plane on which you lived your life.'")
      time.sleep(3)
 
 def who_are_you():
-    time.sleep(5)
+    time.sleep(4)
     print("The mysterious figure speaks: 'I am merely a guardian, a gate-watcher of the realm beyond'")
     time.sleep(1)
     print("'Few who come here get past me, but all they will know beyond this point is suffering'")
     time.sleep(3)
 
 def what_happened_to_me(figure_questions, asking_figure, question):
-    time.sleep(5)
+    time.sleep(4)
     print("The mysterious figure speaks: 'Dear mortal, even though you may not remember it now, you were a bringer of pain and suffering in your realm, with a cruel spirit and a wicked heart.'")
     time.sleep(1)
     print("'But an ally who you thought loyal to you and your cause betrayed you.'")
@@ -87,9 +87,9 @@ def what_happened_to_me(figure_questions, asking_figure, question):
     print("The mysterious figure speaks: 'Well, mortal? Do you wish to proceed?'")
     time.sleep(2)
     print("Hesistant, you would like to ask this mysterious figure one more question: ")
-    print(str(question[0]))
-    print(str(question[1]))
-    print(str(question[3]))
+    print(figure_questions[0])
+    print(figure_questions[1])
+    print(figure_questions[3])
     time.sleep(2) 
     input("What would you like to ask?: ")
 
@@ -103,7 +103,7 @@ def what_happened_to_me(figure_questions, asking_figure, question):
         what_lies_beyond_gate()
 
 def what_lies_beyond_gate():
-     time.sleep(5)
+     time.sleep(4)
 
 
 
